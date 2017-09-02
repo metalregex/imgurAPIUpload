5c1782eb4212339dcc36ab2483292803aeb56f40
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2017-09-02 16:39:24 +0530

    Added one more way to add imgur appid.

--- uploadPicture.js
+++ uploadPicture.js
@@ -1,26 +1,28 @@
 var UPLOAD_URL 	= "https://api.imgur.com/3/image.json";
 
 // Hardcode client id.
-// var CLIENT_ID 	= "xxxxxxxxxxxxxxx";
+var CLIENT_ID_HARD 	= undefined;
 
 // Div for image preview.
 let imgPrevDiv 	= document.querySelector(".img-preview");
+let urlPrev 		= document.querySelector("#uploadedImgUrl");
 
 /**
  * This will be run after all is done.
  */
-var injectUrl = function() {	
-	console.log("[+] Success uploading!");	
-	let res = JSON.parse(this.responseText);	
+var injectUrl = function() {
+	console.log("[+] Success uploading!");
+	let res = JSON.parse(this.responseText);
 	imgPrevDiv.setAttribute("src", res.data.link);
+	urlPrev.value = res.data.link;
 };
 
 /**
  * Should be called onchange="uploadToImgur.call(this)" on a file input
- * element. 
+ * element.
  */
 var uploadToImgur = function() {
-	if ('files' in this && this.files.length > 0) 
+	if ('files' in this && this.files.length > 0)
 		upload(this.files[0], injectUrl);
 };
 
@@ -29,8 +31,10 @@ var uploadToImgur = function() {
  * @param {File}	file 	File to upload.
  */
 var upload = function(file, cb) {
+	// empty url prev data
+	urlPrev.value = "";
 	// Get client id on change
-	var CLIENT_ID = document.querySelector("#client-id").value;
+	var CLIENT_ID = CLIENT_ID_HARD || document.querySelector("#client-id").value;
 
 	if (CLIENT_ID.length < 1) {
 		throw new Error("I need a client Id!");
